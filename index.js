465e8b1765bb92a1287caffae04ee172ae056b20
Hao Yuan <haomichaelyuan@gmail.com>
2022-09-21 14:54:08 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(a="roller-skate"){
+    return `This Saturday, I want to ${a}!`
+}
+
+console.log(saturdayFun())
+
+const mondayWork=function(a="go to the office"){
+    return `This Monday, I will ${a}.`
+}
+
+function wrapAdjective (a="special"){
+
+    return (function(b="*"){return `You are ${a}${b}${a}!`})
+
+}
+console.log(wrapAdjective("%")("a dedicated programmer"))
